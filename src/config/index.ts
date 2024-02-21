556ebd56918ee8b114f399a205576880dc0765fa
jabin-v <jabinv03@gmail.com>
2024-02-22 01:24:40 +0530

    add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,9 @@ import { config } from "dotenv";
 // eslint-disable-next-line
 config();
 
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 export const Config = {
     PORT,
+    NODE_ENV,
 };
