630129bf0ac1d3284740991100e436e5e7b011be
jabin-v <jabinv03@gmail.com>
2024-02-22 00:09:13 +0530

    add express js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     rules: {
         "no-console": "error",
         "dot-notation": "error",
-        // "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/require-await": "off",
     },
 };
