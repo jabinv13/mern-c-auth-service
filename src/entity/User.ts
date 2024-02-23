5b0f92ab2cfacc7f4fda8bc60dd28ce75d3a4058
jabin-v <jabinv03@gmail.com>
2024-02-23 13:35:49 +0530

    setup database

---
+++ src/entity/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "users" })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // @Column()
+    // firstName: string;
+
+    // @Column()
+    // lastName: string;
+
+    // @Column({ unique: true })
+    // email: string;
+
+    // @Column({ select: false })
+    // password: string;
+
+    // @Column()
+    // role: string;
+}
