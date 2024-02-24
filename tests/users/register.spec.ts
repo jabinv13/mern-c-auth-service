69a0b472c85bf3f5c20c34c398fa0c8ac0d2edc7
jabin-v <jabinv03@gmail.com>
2024-02-25 01:25:00 +0530

    test token generation

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import { User } from "../../src/entity/User";
 import { AppDataSource } from "../../src/config/data-source";
 import { Roles } from "../../src/constants";
+import { isJWt } from "../utils";
 // import { truncateTables } from "../utils";
 
 describe("POST /auth/register", () => {
@@ -173,6 +174,47 @@ describe("POST /auth/register", () => {
 
             expect(response.statusCode).toBe(400);
             expect(users).toHaveLength(1);
+        });
+        it("should return the access token and refresh token inside a cookie", async () => {
+            const userData = {
+                firstName: "Jabin",
+                lastName: "v",
+                email: "jabinv@mern001.auth",
+                password: "password",
+            };
+
+            //ACT
+
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            //ASSERT
+
+            interface Headers {
+                ["set-cookie"]: string[];
+            }
+
+            let accessToken: string = "";
+            let refreshToken: string = "";
+
+            const cookies =
+                (response.headers as unknown as Headers)["set-cookie"] || [];
+
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith("accessToken=")) {
+                    accessToken = cookie.split(";")[0].split("=")[1];
+                }
+                if (cookie.startsWith("refreshToken=")) {
+                    refreshToken = cookie.split(";")[0].split("=")[1];
+                }
+            });
+
+            expect(accessToken).not.toBeNull();
+            expect(refreshToken).not.toBeNull();
+
+            expect(isJWt(accessToken)).toBeTruthy();
+            expect(isJWt(refreshToken)).toBeTruthy();
         });
     });
 
